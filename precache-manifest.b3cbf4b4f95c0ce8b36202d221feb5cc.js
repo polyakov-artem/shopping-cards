b579c7f4276fc071b3c19c4a75efee0f777c83a6
polyakov-artem <Polyakov.Artem.V@yandex.ru>
2019-09-10 17:41:26 +0400

    Updates

---
+++ precache-manifest.b3cbf4b4f95c0ce8b36202d221feb5cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8efb88f060e2d33482fbdb304b4fc3ae",
+    "url": "/shopping-cards/index.html"
+  },
+  {
+    "revision": "fcf0c2c1da81b0fd299b",
+    "url": "/shopping-cards/static/css/main.ebcb51c1.chunk.css"
+  },
+  {
+    "revision": "0d1fe33cf862016217e5",
+    "url": "/shopping-cards/static/js/2.7a2b8d10.chunk.js"
+  },
+  {
+    "revision": "fcf0c2c1da81b0fd299b",
+    "url": "/shopping-cards/static/js/main.5d61b124.chunk.js"
+  },
+  {
+    "revision": "d03ea436950c18832ad1",
+    "url": "/shopping-cards/static/js/runtime~main.f29a2139.js"
+  }
+]);
